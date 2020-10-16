5d3e84b17aa125c8bf92e2f79ae8f3bbb10f84bf
Tony Andr <blueberry.acc@gmail.com>
2020-10-16 09:17:14 +0200

    fixes

--- api/task/models/task.js
+++ api/task/models/task.js
@@ -6,36 +6,51 @@
  */
 
 const createArticleLogic = async (data) => {
-    console.log(data);
+    // console.log(data);
 
-    let parentId = data.id;
-    if (data.keywords) {
-        let kws = data.keywords.trim().split('\n');
-    
-        for (let i = 0; i < kws.length; i++) {
-            let kwPair = kws[i].split('\t');
-    
-            let exists = await strapi.services.article.findOne({ task: parentId, keyword: kwPair[0].trim() });
-            if (!exists) {
-                let article = {
-                    keyword: kwPair[0].trim(),
-                    task: parentId,
-                    category: kwPair[1].trim()
+    try {
+        let parentId = data.id;
+        if (data.keywords) {
+            let kws = data.keywords.trim().split('\n');
+
+            for (let i = 0; i < kws.length; i++) {
+                let kwPair = kws[i].split('\t');
+
+                let exists = await strapi.services.article.findOne({ task: parentId, keyword: kwPair[0].trim() });
+                if (!exists) {
+                    let article = {
+                        keyword: kwPair[0].trim(),
+                        task: parentId,
+                        category: kwPair[1].trim()
+                    }
+                    await strapi.services.article.create(article);
                 }
-                await strapi.services.article.create(article);
             }
+            
         }
-        await strapi.services.task.update({ id: data.id }, { keywords: '' });
+        return true;
+    } catch (err) {
+        console.log(err);
+        return false;
     }
+
 }
 
 module.exports = {
     lifecycles: {
         async afterCreate(data) {
-            await createArticleLogic(data);       
+            createArticleLogic(data).then(res => {
+                if (res) {
+                    strapi.services.task.update({ id: data.id }, { keywords: '' });
+                }
+            });       
         },
         async afterUpdate(result, params, data) {
-            await createArticleLogic(data);
+            createArticleLogic(data).then(res => {
+                if (res) {
+                    strapi.services.task.update({ id: data.id }, { keywords: '' });
+                }
+            });
         }
     },
 };
